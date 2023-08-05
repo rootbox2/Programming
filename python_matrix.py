50a0ea7c60a2f6bcbd35da4fb4481f41d6119398
rotbox2 <borno2002.blackstone@gmail.com>
2023-08-05 22:53:22 +0600

    08/05/2023
    python matrix practise

---
+++ python_matrix.py
@@ -0,0 +1,11 @@
+X = [
+    [1, 2, 3, 4, 5, 6, 7],
+    ['A', 'B', 'C', 'D', 'E', 'F'],
+    ['Facebook', 'Youtube', 'Whatsapp', 'Viber', 'Imo', 'Teligram']
+]
+
+
+# Data add in matrix 
+X.append(300)
+
+print('Append List : ',X)
